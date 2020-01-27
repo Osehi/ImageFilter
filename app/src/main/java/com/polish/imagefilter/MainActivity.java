78961df641b1e279628fc6ac8a7449f39fae1ddd
Osehi <osehiaseehilen@gmail.com>
2020-01-27 02:12:50 +0100

    'ui-created'

--- app/src/main/java/com/polish/imagefilter/MainActivity.java
+++ app/src/main/java/com/polish/imagefilter/MainActivity.java
@@ -1,14 +1,35 @@
 package com.polish.imagefilter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+
+    }
+
+    // a method  to pull up a photo
+    public void choosePhoto(View v){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
