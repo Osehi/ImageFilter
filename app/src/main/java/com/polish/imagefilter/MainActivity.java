8f92fd1204c847f7905b1e954e7ab029974bc833
Osehi <osehiaseehilen@gmail.com>
2020-01-27 02:30:44 +0100

    'get-image'

--- app/src/main/java/com/polish/imagefilter/MainActivity.java
+++ app/src/main/java/com/polish/imagefilter/MainActivity.java
@@ -3,14 +3,27 @@ package com.polish.imagefilter;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    // image object to load images
+    private Bitmap image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +44,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == Activity.RESULT_OK && data != null){
+            try {
+
+                Uri uri = data.getData();
+                ParcelFileDescriptor parcelFileDescriptor =
+                        getContentResolver().openFileDescriptor(uri, "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                // load my image
+                imageView.setImageBitmap(image);
+
+
+            }
+            catch (IOException e){
+                Log.e("imageFilter", "Image not found", e);
+            }
+
+        }
+
     }
 }
